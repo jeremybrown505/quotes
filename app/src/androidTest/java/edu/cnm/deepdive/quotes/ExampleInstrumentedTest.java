630af082b7b87f81f21eac4d30c45f4a6616adb9
Jeremy Brown <Jeremy505brown@gmail.com>
2020-06-17 16:35:04 -0600

    got all the quotes to display in the emulator.

--- app/src/androidTest/java/edu/cnm/deepdive/quotes/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/cnm/deepdive/quotes/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
     // Context of the app under test.
     Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-    assertEquals("edu.cnm.deepdive.quotes", appContext.getPackageName());
+    assertEquals("edu.cnm.deepdive.fragment_quotes", appContext.getPackageName());
   }
 }
