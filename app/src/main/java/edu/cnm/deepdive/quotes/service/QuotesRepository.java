8f60584411a6c8448b1d99868b6900dc3c843fa5
Jeremy Brown <Jeremy505brown@gmail.com>
2020-06-29 17:00:21 -0600

    added onClickListener and changed visual layout, added button to change text of source/quote.

--- app/src/main/java/edu/cnm/deepdive/quotes/service/QuotesRepository.java
+++ app/src/main/java/edu/cnm/deepdive/quotes/service/QuotesRepository.java
@@ -28,6 +28,8 @@ public class QuotesRepository {
         .subscribeOn(Schedulers.io());
   }
 
-  // TODO Add other methods as necessary.
-
+  public Single<QuoteWithSource> getQuote(long id) {
+    return quoteDao.selectById(id)
+        .subscribeOn(Schedulers.io());
+  }
 }
