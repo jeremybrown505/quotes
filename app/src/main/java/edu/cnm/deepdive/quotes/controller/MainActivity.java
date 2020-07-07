406a8b1c89af12da82f95b078af0ba6c677003c7
Jeremy Brown <Jeremy505brown@gmail.com>
2020-07-07 14:50:19 -0600

    Implemented Google Sign In.

--- app/src/main/java/edu/cnm/deepdive/quotes/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/quotes/controller/MainActivity.java
@@ -1,23 +1,56 @@
 package edu.cnm.deepdive.quotes.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
-import androidx.lifecycle.ViewModelProvider;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import edu.cnm.deepdive.quotes.R;
+import edu.cnm.deepdive.quotes.service.GoogleSignInService;
 import edu.cnm.deepdive.quotes.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+  private GoogleSignInService signInService;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    setupNavigation();
+    setupObservers();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.options, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    //noinspection SwitchStatementWithTooFewBranches
+    switch (item.getItemId()) {
+      case R.id.sign_out:
+        signInService.signOut().addOnCompleteListener((ignored) -> switchToLogin());
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+    }
+    return handled;
+  }
+
+  private void setupNavigation() {
     BottomNavigationView navView = findViewById(R.id.nav_view);
     // Passing each menu ID as a set of Ids because each
     // menu should be considered as top level destinations.
@@ -27,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     NavigationUI.setupWithNavController(navView, navController);
+  }
+
+  private void setupObservers() {
     MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
     getLifecycle().addObserver(viewModel);
     viewModel.getThrowable().observe(this, (throwable) -> {
@@ -34,6 +70,13 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_LONG).show();
       }
     });
+    signInService = GoogleSignInService.getInstance();
+  }
+
+  private void switchToLogin() {
+    Intent intent = new Intent(this, LoginActivity.class);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+    startActivity(intent);
   }
 
 }
